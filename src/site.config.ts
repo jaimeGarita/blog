e290a1b9085b77cdb1b60bc16573c5489e83cd86
jaimeGarita <jaime.garita.camargo@gmail.com>
2024-06-25 03:01:23 +0200

    more changes in meta info

--- src/site.config.ts
+++ src/site.config.ts
@@ -24,8 +24,7 @@ export const siteConfig: SiteConfig = {
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
 	title: "Blog Jaime Garitagoitia",
 	webmentions: {
-		// Webmention.io API endpoint. Get your own here: https://webmention.io/, and follow this blog post: https://astro-cactus.chriswilliams.dev/posts/webmentions/
-		link: "https://webmention.io/astro-cactus.chriswilliams.dev/webmention",
+		link: "https://webmention.io/blog.jaimegarita.com/webmention",
 	},
 };
 
